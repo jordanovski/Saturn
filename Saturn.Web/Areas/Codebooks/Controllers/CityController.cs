52036fe2afc062eddfc8244d6bf019c0f30a1e54
Angel <jordanovski.angel@gmail.com>
2014-06-14 15:49:08 +0200

    City finished

--- Saturn.Web/Areas/Codebooks/Controllers/CityController.cs
+++ Saturn.Web/Areas/Codebooks/Controllers/CityController.cs
@@ -1,6 +1,7 @@
 ﻿using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 using Saturn.Data;
+using Saturn.Interface.Repository;
 using Saturn.Model.Codebooks;
 using Saturn.Repository;
 using System.Net;
@@ -11,7 +12,19 @@ namespace Saturn.Web.Areas.Codebooks.Controllers
 {
     public class CityController : Controller
     {
-        readonly CityRepository repository = new CityRepository(new SaturnDbContext());
+        private readonly ICityRepository repository;
+
+        public CityController()
+        {
+            this.repository = new CityRepository(new SaturnDbContext());
+
+        }
+        public CityController(ICityRepository repository)
+        {
+            this.repository = repository;
+
+        }
+
 
         public ActionResult Index()
         {
